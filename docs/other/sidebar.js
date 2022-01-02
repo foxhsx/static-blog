6660ab4c064927d888ac6fbda0a064d599087dea
foxhsx <932163475@qq.com>
2022-01-02 21:47:50 +0800

    add 总结

--- docs/other/sidebar.js
+++ docs/other/sidebar.js
@@ -1,12 +1,13 @@
 module.exports = [
   {
-    text: '随笔',
+    text: "随笔",
     collapsable: true,
-    path: '/docs/other/',
+    path: "/docs/other/",
     children: [
-      { text: '西安探店——西安方所', link: '/docs/other/notes/XianFangsuo'},
-      { text: '微信公众号相关', link: '/docs/other/notes/wechat'},
-      { text: '你好，云窑', link: '/docs/other/notes/hello_yao'},
-    ]
-  }
-]
+      { text: "西安探店——西安方所", link: "/docs/other/notes/XianFangsuo" },
+      { text: "微信公众号相关", link: "/docs/other/notes/wechat" },
+      { text: "你好，云窑", link: "/docs/other/notes/hello_yao" },
+      { text: "2021 年度总结", link: "/docs/other/notes/2021_zongjie" },
+    ],
+  },
+];
