c97ff9c487e03bc6b088c1fd28abeedfc10a97b7
foxhsx <932163475@qq.com>
2022-01-04 22:41:42 +0800

    修改图片及添加网络面试题

--- docs/baodian/high/http.sidebar.js
+++ docs/baodian/high/http.sidebar.js
@@ -1,9 +1,13 @@
 module.exports = [
-    {
-      text: '网络面试',
-      collapsable: true,
-      children: [
-        { text: '什么是跨域？跨域的解决方案', link: '/docs/baodian/high/notes/http/cross' },
-      ]
-    }
-  ]
+  {
+    text: "网络面试",
+    collapsable: true,
+    children: [
+      {
+        text: "什么是跨域？跨域的解决方案",
+        link: "/docs/baodian/high/notes/http/cross",
+      },
+      { text: "网络分层的含义", link: "/docs/baodian/high/notes/http/osi" },
+    ],
+  },
+];
