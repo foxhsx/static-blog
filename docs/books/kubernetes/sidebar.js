9fe5fea277ce2c42ffbaf68e179d697191b0a900
foxhsx <932163475@qq.com>
2022-02-20 22:06:52 +0800

    Pod 概念

--- docs/books/kubernetes/sidebar.js
+++ docs/books/kubernetes/sidebar.js
@@ -1,28 +1,32 @@
 module.exports = [
   {
-    text: "深入剖析 Kubernetes",
+    text: '深入剖析 Kubernetes',
     children: [
-      { text: "1.小鲸鱼大事记", link: "/docs/books/kubernetes/part_1" },
-      { text: "2.白话容器基础", link: "/docs/books/kubernetes/part_2" },
+      { text: '1.小鲸鱼大事记', link: '/docs/books/kubernetes/part_1' },
+      { text: '2.白话容器基础', link: '/docs/books/kubernetes/part_2' },
       {
-        text: "3.谈谈 Kubernetes 的本质",
-        link: "/docs/books/kubernetes/part_3",
+        text: '3.谈谈 Kubernetes 的本质',
+        link: '/docs/books/kubernetes/part_3',
       },
       {
-        text: "4.一键部署 K8S 利器：kubeadm",
-        link: "/docs/books/kubernetes/part_4",
+        text: '4.一键部署 K8S 利器：kubeadm',
+        link: '/docs/books/kubernetes/part_4',
       },
       {
-        text: "5.搭建一个完整的 K8S 集群",
-        link: "/docs/books/kubernetes/part_5",
+        text: '5.搭建一个完整的 K8S 集群',
+        link: '/docs/books/kubernetes/part_5',
       },
       {
-        text: "6.第一个容器化应用",
-        link: "/docs/books/kubernetes/part_6",
+        text: '6.第一个容器化应用',
+        link: '/docs/books/kubernetes/part_6',
       },
       {
-        text: "7.为什么我们需要 Pod?",
-        link: "/docs/books/kubernetes/part_7",
+        text: '7.为什么我们需要 Pod?',
+        link: '/docs/books/kubernetes/part_7',
+      },
+      {
+        text: '8.深入解析 Pod 对象之基本概念',
+        link: '/docs/books/kubernetes/part_8',
       },
     ],
   },
